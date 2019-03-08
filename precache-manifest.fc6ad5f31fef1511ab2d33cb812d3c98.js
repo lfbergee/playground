39817369d854473cff9d43d1a1b3bf5f691c66b5
Leiv Fredrik Berge <leiv.fredrik.berge@sparebank1.no>
2019-03-08 14:50:36 +0100

    Updates

---
+++ precache-manifest.fc6ad5f31fef1511ab2d33cb812d3c98.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ca033c7e26b24305011",
+    "url": "/playground/static/js/6.5ca033c7.chunk.js"
+  },
+  {
+    "revision": "ce54b5b65c53f61a15ec",
+    "url": "/playground/static/js/0.ce54b5b6.chunk.js"
+  },
+  {
+    "revision": "58c62cd9377ab17cf448",
+    "url": "/playground/static/js/runtime~main.58c62cd9.js"
+  },
+  {
+    "revision": "c1d36a1d29d9d3dae23c",
+    "url": "/playground/static/js/3.c1d36a1d.chunk.js"
+  },
+  {
+    "revision": "4bf318cd0db30348c654",
+    "url": "/playground/static/js/4.4bf318cd.chunk.js"
+  },
+  {
+    "revision": "6da69666c89aa9f7e159",
+    "url": "/playground/static/js/5.6da69666.chunk.js"
+  },
+  {
+    "revision": "d51a200ff6831b2b4868",
+    "url": "/playground/static/js/main.d51a200f.chunk.js"
+  },
+  {
+    "revision": "16ce3cdde77dca574563",
+    "url": "/playground/static/js/7.16ce3cdd.chunk.js"
+  },
+  {
+    "revision": "0ab1c3bc927e3ffc96e2",
+    "url": "/playground/static/js/8.0ab1c3bc.chunk.js"
+  },
+  {
+    "revision": "15a2cd96368b05095da5",
+    "url": "/playground/static/js/9.15a2cd96.chunk.js"
+  },
+  {
+    "revision": "9d893a1e9c859d8defe930931eec155f",
+    "url": "/playground/static/media/bg.9d893a1e.jpg"
+  },
+  {
+    "revision": "580bbc8c8bc7bc93448cc9c9ba7ad5b7",
+    "url": "/playground/index.html"
+  }
+];
